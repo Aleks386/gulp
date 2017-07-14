3c2beee1177933555bc21ef28c36fc833adbb8d9
Work User <puhkiy@gmai.com>
2017-07-14 17:38:59 +0300

    all

--- gulp/project/gulpfile.js
+++ gulp/project/gulpfile.js
@@ -3,7 +3,11 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
 	gulp.src('./project/**/*.scss')
-		.pipe(sass())
+		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./project/css'))
 	
+});
+
+gulp.task('sass:watch', function(){
+	gulp.watch('./project/**/*.scss', ['sass']);
 });
