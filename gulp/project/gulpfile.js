42a452b7543633483ed91c62687f843477c3e6ed
Work User <puhkiy@gmai.com>
2017-07-19 18:23:54 +0300

    new chages

--- gulp/project/gulpfile.js
+++ gulp/project/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-	gulp.src('./project/**/*.scss')
+	gulp.src('./project/scss/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./project/css'))
 	
